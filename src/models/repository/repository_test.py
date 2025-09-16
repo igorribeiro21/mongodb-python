450ab33d91f2ced91451ad15e7412ff19a4c46f8
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-09-15 21:32:03 -0300

    feat: add selection methods to OrdersRepository and corresponding tests

--- src/models/repository/repository_test.py
+++ src/models/repository/repository_test.py
@@ -1,3 +1,4 @@
+import pytest
 from src.models.connection.connection_handler import DBConnectionHandler
 from .orders_repository import OrdersRepository
 
@@ -5,12 +6,55 @@ db_connection_handler = DBConnectionHandler()
 db_connection_handler.connect_to_db()
 conn = db_connection_handler.get_db_connection()
 
+@pytest.mark.skip(reason="Interação com banco")
 def test_insert_document():
     orders_repository = OrdersRepository(conn)
     my_doc = { "ola": "mundo" }
     orders_repository.insert_document(my_doc)
 
+@pytest.mark.skip(reason="Interação com banco")
 def test_insert_list_of_documents():
     orders_repository = OrdersRepository(conn)
     my_doc = [{ "elem1": "aqui1" }, { "elem2": "aqui2" }, { "elem3": "aqui3" }]
     orders_repository.insert_list_of_documents(my_doc)
+
+@pytest.mark.skip(reason="Interação com banco")
+def test_select_many():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom": True }
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+    for doc in response:
+        print(doc)
+        print()
+        print(doc["itens"])
+        print()
+
+@pytest.mark.skip(reason="Interação com banco")
+def test_select_one():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom": True }
+    response = orders_repository.select_one(doc_filter)
+    print()
+    print(response)
+
+@pytest.mark.skip(reason="Interação com banco")
+def test_select_many_with_properties():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom": True }
+    response = orders_repository.select_many_with_properties(doc_filter)
+    print()
+    print(response)
+    for doc in response:
+        print(doc)
+        print()
+
+def test_select_if_properties_exists():
+    orders_repository = OrdersRepository(conn)
+    response = orders_repository.select_if_properties_exists()
+    print()
+    print(response)
+    for doc in response:
+        print(doc)
+        print()
